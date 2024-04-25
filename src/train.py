78cbc9c9db7e11e437e887b4367ef63a1ccf019f
Mo <mohamedabdelhamid3030@gmail.com>
2024-04-25 14:02:14 +0200

    updte resource tracker

--- src/train.py
+++ src/train.py
@@ -20,7 +20,7 @@ from utils import (
     read_json_as_dict,
     set_seeds,
     train_test_split,
-    TimeAndMemoryTracker,
+    ResourceTracker,
 )
 
 logger = get_logger(task_name="train")
@@ -63,7 +63,7 @@ def run_training(
 
     try:
 
-        with TimeAndMemoryTracker(logger) as _:
+        with ResourceTracker(logger, monitoring_interval=0.1):
             logger.info("Starting training...")
             # load and save schema
             logger.info("Loading and saving schema...")
